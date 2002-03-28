6d8fb85a6e34a30a98db9ec5786f1aad6276cfce
chappell <>
2002-03-28 16:38:17 +0000

    *** empty log message ***

--- makeprogs/save_symlinks.sh
+++ makeprogs/save_symlinks.sh
@@ -1,4 +1,10 @@
 #! /bin/sh
+#
+# mouse:~ppr/src/makeprogs/save_symlinks.sh
+# Copyright 1995--2002, Trinity College Computing Center.
+# Written by David Chappell.
+# Last modified 28 March 2002.
+#
 
 exec >.restore_symlinks
 chmod 755 .restore_symlinks
@@ -9,8 +15,8 @@ for link in `find . -type l`
     contents=`readlink $link`
 
     # -h doesn't seem to work with some versions of bash.
-    #echo "if [ ! -h $link ]; then ln -s $contents $link || exit 1; fi"
-    echo "if [ ! -f $link ]; then ln -s $contents $link || exit 1; fi"
+    #echo "if [ ! -h $link ]; then ln -sf $contents $link || exit 1; fi"
+    echo "if [ ! -f $link ]; then ln -sf $contents $link || exit 1; fi"
 
     done
 
