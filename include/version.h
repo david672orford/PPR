47810b13f6bf6ae7bb002931922c94a21f5e9c35
chappell <>
2003-01-23 18:08:57 +0000

    i refuse

--- include/version.h
+++ include/version.h
@@ -1,4 +1,4 @@
-#define VERSION "PPR version 1.50, 22 January 2003 (built "__DATE__", "__TIME__")."
+#define VERSION "PPR version 1.50, 23 January 2003 (built "__DATE__", "__TIME__")."
 #define SHORT_VERSION "1.50"
 #define COPYRIGHT "Copyright 1995--2002, Trinity College Computing Center."
 #define AUTHOR "Written by David Chappell."
