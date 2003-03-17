f4cbb12248ffe9b3ede1acce981075e38b6238f1
chappell <>
2003-03-17 20:49:36 +0000

    no_comment_mouse

--- include/version.h
+++ include/version.h
@@ -1,5 +1,5 @@
 #define SHORT_VERSION "1.51"
-#define REVISION_DATE "14 March 2003"
+#define REVISION_DATE "17 March 2003"
 #define COPYRIGHT_YEARS "1995--2003"
 
 #define VERSION "PPR version "SHORT_VERSION", "REVISION_DATE" (built "__DATE__", "__TIME__")."
--- libppr/destinfo.c
+++ libppr/destinfo.c
@@ -1,6 +1,6 @@
 /*
 ** mouse:~ppr/src/libppr/destinfo.c
-** Copyright 1995--2002, Trinity College Computing Center.
+** Copyright 1995--2003, Trinity College Computing Center.
 ** Written by David Chappell.
 **
 ** Redistribution and use in source and binary forms, with or without
@@ -25,11 +25,12 @@
 ** ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 ** POSSIBILITY OF SUCH DAMAGE.
 **
-** Last modified 5 August 2002.
+** Last modified 17 March 2003.
 */
 
 #include "before_system.h"
 #include <stdio.h>
+#include <string.h>
 #ifdef INTERNATIONAL
 #include <libintl.h>
 #endif
@@ -39,22 +40,67 @@
 const char *dest_ppdfile(const char destnode[], const char destname[])
     {
     char fname[MAX_PPR_PATH];
-    FILE *f;
+    FILE *f = NULL;
     char *line = NULL;
     int line_space = 128;
     char *ppdfile = NULL;
+    char *p;
+    char *freeme = NULL;
 
+    /* If the specified destination is an alias, replace destname with the thing
+       which the alias points to. */
+    ppr_fnamef(fname, "%s/%s", ALIASCONF, destname);
+    if((f = fopen(fname, "r")))
+	{
+	while((line = gu_getline(line, &line_space, f)))
+	    {
+	    if((p = lmatchp(line, "ForWhat:")))
+	    	{
+	    	destname = freeme = gu_strdup(p);
+	    	break;
+	    	}
+	    }
+	}
+
+    /* If the specified destination (as possibly dealiased above) is a group, take its
+       first member.
+       */
+    ppr_fnamef(fname, "%s/%s", GRCONF, destname);
+    if((f = fopen(fname, "r")))
+	{
+	while((line = gu_getline(line, &line_space, f)))
+	    {
+	    if((p = lmatchp(line, "Printer:")))
+		{
+		if(freeme)
+		    gu_free(freeme);
+	    	destname = freeme = gu_strdup(p);
+		break;
+	    	}
+	    }
+	}
+	
+    /* At this point we should have a printer name.  We will open the 
+       configuration file and extract the PPD file name.
+       */
     ppr_fnamef(fname, "%s/%s", PRCONF, destname);
     if((f = fopen(fname, "r")))
 	{
 	while((line = gu_getline(line, &line_space, f)))
 	    {
-	    if(!ppdfile && gu_sscanf(line, "PPDFile: %Z", &ppdfile) == 1)
+	    if(gu_sscanf(line, "PPDFile: %Z", &ppdfile) == 1)
 	    	break;
 	    }	
-	if(line)
-	    gu_free(line);
 	}
+
+    if(line)
+	gu_free(line);
+
+    if(f)
+	fclose(f);
+
+    if(freeme)
+	gu_free(freeme);
 
     return ppdfile;
     }
