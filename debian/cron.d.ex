04ee158ad55b15196f91da2d041330bce9d041db
chappell <>
2004-05-11 21:33:31 +0000

    no_comment_mouse

---
+++ debian/cron.d.ex
@@ -0,0 +1,4 @@
+#
+# Regular cron jobs for the ppr package
+#
+0 4	* * *	root	ppr_maintenance
---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(ppr):needs=X11|text|vc|wm section=Apps/see-menu-manual\
+  title="ppr" command="/usr/bin/ppr"
