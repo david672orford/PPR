8255298052c89adc23380ccb71767c992c60ceb0
chappell <>
2005-03-24 20:09:21 +0000

    no_comment_mouse

---
+++ libgu/gu_name_value.c
@@ -0,0 +1,54 @@
+/*
+** mouse:~ppr/src/libgu/gu_name_value.c
+** Copyright 1995--2005, Trinity College Computing Center.
+** Written by David Chappell.
+**
+** Redistribution and use in source and binary forms, with or without
+** modification, are permitted provided that the following conditions are met:
+**
+** * Redistributions of source code must retain the above copyright notice,
+** this list of conditions and the following disclaimer.
+**
+** * Redistributions in binary form must reproduce the above copyright
+** notice, this list of conditions and the following disclaimer in the
+** documentation and/or other materials provided with the distribution.
+**
+** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+** AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+** IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+** ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+** LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+** CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+** SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+** INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+** CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+** ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+** POSSIBILITY OF SUCH DAMAGE.
+**
+** Last modified 24 March 2005.
+*/
+
+/*! \file */
+
+#include "config.h"
+#include "gu.h"
+
+/** format name and integer value in allocated memory
+ */
+char *gu_name_int_value(const char name[], int value)
+	{
+	char *mem;
+	gu_asprintf(&mem, "%s=%d", name, value);
+	return mem;
+	}
+
+/** format name and string value in allocated memory
+ */
+char *gu_name_str_value(const char name[], const char value[])
+	{
+	char *mem;
+	gu_asprintf(&mem, "%s=%s", name, value);
+	return mem;
+	}
+
+/* end of file */
