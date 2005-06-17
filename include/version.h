e1a0b2f90592ffeef387f3cd455260a2251f4fa5
chappell <>
2005-06-17 19:30:11 +0000

    no_comment_mouse

--- include/version.h
+++ include/version.h
@@ -1,5 +1,5 @@
 #define SHORT_VERSION "2.00a1"
-#define REVISION_DATE "16 June 2005"
+#define REVISION_DATE "17 June 2005"
 #define COPYRIGHT_YEARS "1995--2005"
 
 #define VERSION "PPR version "SHORT_VERSION", "REVISION_DATE" (built "__DATE__", "__TIME__")."
