9e18227efcf2b693e905eee43b9e7db415a37524
chappell <>
2005-09-10 03:03:29 +0000

    no_comment_mouse

---
+++ libgu/gu_isxdigit.c
@@ -0,0 +1,115 @@
+/*
+** mouse:~ppr/src/libgu/gu_isxdigit.c
+** Copyright 1995--2005, Trinity College Computing Center.
+** Written by David Chappell.
+**
+** Redistribution and use in source and binary forms, with or without
+** modification, are permitted provided that the following conditions are met:
+**
+** * Redistributions of source code must retain the above copyright notice,
+** this list of conditions and the following disclaimer.
+**
+** * Redistributions in binary form must reproduce the above copyright
+** notice, this list of conditions and the following disclaimer in the
+** documentation and/or other materials provided with the distribution.
+**
+** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+** AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+** IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+** ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+** LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+** CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+** SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+** INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+** CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+** ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+** POSSIBILITY OF SUCH DAMAGE.
+**
+** Last modified 2 September 2005.
+*/
+
+#include "config.h"
+#ifdef INTERNATIONAL
+#include <libintl.h>
+#endif
+#include "gu.h"
+#include "global_defines.h"
+
+/** Determine if a character is an ASCII hexadecimal digit
+ *
+** This function replaces isdigit() from the C library.  The one in the C library 
+** determines if c is a digit in the current locale.  We want to know if it is a
+** digit in ASCII.
+*/
+int gu_ascii_isxdigit(int c)
+	{
+	switch(c)
+		{
+		case '0':
+		case '1':
+		case '2':
+		case '3':
+		case '4':
+		case '5':
+		case '6':
+		case '7':
+		case '8':
+		case '9':
+		case 'a':
+		case 'b':
+		case 'c':
+		case 'd':
+		case 'e':
+		case 'f':
+		case 'A':
+		case 'B':
+		case 'C':
+		case 'D':
+		case 'E':
+		case 'F':
+			return 1;
+		default:
+			return 0;
+		}
+	}
+
+/** Returns the value of an ASCII hex digit
+ *
+ * This function returns the numberic value of an ASCII hex digit.  If the
+ * character is not an ASCII hex digit, it returns -1.
+ */
+int gu_ascii_xdigit_value(int c)
+	{
+	switch(c)
+		{
+		case '0':
+		case '1':
+		case '2':
+		case '3':
+		case '4':
+		case '5':
+		case '6':
+		case '7':
+		case '8':
+		case '9':
+			return c - '0';
+		case 'a':
+		case 'b':
+		case 'c':
+		case 'd':
+		case 'e':
+		case 'f':
+			return c - 'a' + 10;
+		case 'A':
+		case 'B':
+		case 'C':
+		case 'D':
+		case 'E':
+		case 'F':
+			return c - 'A' + 10;
+		default:
+			return -1;
+		}
+	}
+
+/* end of file */
