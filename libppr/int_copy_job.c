af43d0f045341916acfb6385eace436f182f2fda
chappell <>
2006-02-16 15:38:02 +0000

    no_comment_mouse

--- libppr/int_copy_job.c
+++ libppr/int_copy_job.c
@@ -25,7 +25,7 @@
 ** ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 ** POSSIBILITY OF SUCH DAMAGE.
 **
-** Last modified 15 February 2006.
+** Last modified 16 February 2006.
 */
 
 /*! \file
