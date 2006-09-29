ac254581b0697c01eddaca785d51043604dae295
chappell <>
2006-09-29 14:14:42 +0000

    *** empty log message ***

--- samba/indexwindrivers.perl
+++ samba/indexwindrivers.perl
@@ -1,32 +1,14 @@
 #! @PERL_PATH@ -w
 #
 # mouse:~ppr/src/samba/indexwindrivers.perl
-# Copyright 1995--2005, Trinity College Computing Center.
+# Copyright 1995--2006, Trinity College Computing Center.
 # Written by David Chappell.
 #
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-# 
-# * Redistributions of source code must retain the above copyright notice,
-# this list of conditions and the following disclaimer.
-# 
-# * Redistributions in binary form must reproduce the above copyright
-# notice, this list of conditions and the following disclaimer in the
-# documentation and/or other materials provided with the distribution.
-# 
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE 
-# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
-# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
-# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
-# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
-# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
-# POSSIBILITY OF SUCH DAMAGE.
+# This file is part of PPR.  You can redistribute it and modify it under the
+# terms of the revised BSD licence (without the advertising clause) as
+# described in the accompanying file LICENSE.txt.
 #
-# Last modified 20 October 2005.
+# Last modified 6 September 2006.
 #
 
 #
@@ -375,7 +357,11 @@ while(defined(my $index_line = <INDEX>))
 	print STDERR "\n" if($opt_verbose);
 	} # end of PPD file iteration
 
-close(DRIVERS) || die $!;
+if(!close(DRIVERS))
+	{
+	print STDERR "$SAMBAPRINT failed: exit code ", $? >> 8, "\n";	
+	exit(10);
+	}
 close(INDEX) || die $!;
 
 print STDERR "Done.\n" if($opt_verbose);
--- z_install_end/install_crontab.sh
+++ z_install_end/install_crontab.sh
@@ -1,32 +1,14 @@
 #! /bin/sh
 #
 # mouse:~ppr/src/z_install_end/install_crontab.sh
-# Copyright 1995--2005, Trinity College Computing Center.
+# Copyright 1995--2006, Trinity College Computing Center.
 # Written by David Chappell.
 #
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
+# This file is part of PPR.  You can redistribute it and modify it under the
+# terms of the revised BSD licence (without the advertising clause) as
+# described in the accompanying file LICENSE.txt.
 #
-# * Redistributions of source code must retain the above copyright notice,
-# this list of conditions and the following disclaimer.
-# 
-# * Redistributions in binary form must reproduce the above copyright
-# notice, this list of conditions and the following disclaimer in the
-# documentation and/or other materials provided with the distribution.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE 
-# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
-# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
-# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
-# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
-# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
-# POSSIBILITY OF SUCH DAMAGE.
-#
-# Last modified 17 January 2005.
+# Last modified 17 August 2006.
 #
 
 . ../makeprogs/paths.sh
@@ -49,8 +31,14 @@ if [ "`../z_install_begin/id -un`" = "$USER_PPR" ]
 	echo "    OK"
 	else
 	echo "    Nope, guess we are root, doing su $USER_PPR..."
-	su $USER_PPR -c $0	  
-	exit $?
+	echo "su $USER_PPR -c $0"
+	su $USER_PPR -c $0
+	su_exit=$?
+	if [ $su_exit -ne 0 ]
+		then
+		echo "Failed."
+		fi
+	exit $su_exit
 	fi
 
 echo "  Installing crontab for the user $USER_PPR..."
