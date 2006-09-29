9a7b497448ca7dbe4e8c2450cb9d11862aed198c
chappell <>
2006-09-29 17:21:26 +0000

    *** empty log message ***

--- docs/rules.mk
+++ docs/rules.mk
@@ -1,31 +1,13 @@
 #
 # mouse:~ppr/src/docs/rules.mk
-# Copyright 1995--2005, Trinity College Computing Center.
+# Copyright 1995--2006, Trinity College Computing Center.
 # Written by David Chappell.
 #
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
+# This file is part of PPR.  You can redistribute it and modify it under the
+# terms of the revised BSD licence (without the advertising clause) as
+# described in the accompanying file LICENSE.txt.
 #
-# * Redistributions of source code must retain the above copyright notice,
-# this list of conditions and the following disclaimer.
-#
-# * Redistributions in binary form must reproduce the above copyright
-# notice, this list of conditions and the following disclaimer in the
-# documentation and/or other materials provided with the distribution.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
-# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-#
-# Last modified 12 October 2005.
+# Last modified 29 September 2006.
 #
 
 # Where do we install the documentation?
@@ -82,10 +64,10 @@ SGML_CATALOG_FILES=../../nonppr_misc/docbook-xml/docbook.cat
 #============================================================================
 
 .html.ps:
-	-$(HTMLDOC) --no-toc --browserwidth 1024 -t ps --outfile $*.ps $*.html
+	$(HTMLDOC) --no-toc --browserwidth 1024 -t ps --outfile $*.ps $*.html
 
 .html.pdf:
-	-$(HTMLDOC) --no-toc --browserwidth 1024 -t pdf12 --compression=9 --outfile $*.pdf $*.html
+	$(HTMLDOC) --no-toc --browserwidth 1024 -t pdf12 --compression=9 --outfile $*.pdf $*.html
 
 #============================================================================
 # Rules to convert Docbook SGML to PostScript and PDF by way
