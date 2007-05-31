b3c74200e7fab4fd85fe567fc1e11a9055296704
chappell <>
2007-05-31 14:02:17 +0000

    no_comment_raven

--- z_install_end/install_sample_config.sh
+++ z_install_end/install_sample_config.sh
@@ -1,31 +1,13 @@
 #
 # mouse:~ppr/src/z_install_end/install_sample_to_conf.sh
-# Copyright 1995--2005, Trinity College Computing Center.
+# Copyright 1995--2006, Trinity College Computing Center.
 # Written by David Chappell.
 #
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
+# This file is part of PPR.  You can redistribute it and modify it under the
+# terms of the revised BSD licence (without the advertising clause) as
+# described in the accompanying file LICENSE.txt.
 #
-# * Redistributions of source code must retain the above copyright notice,
-# this list of conditions and the following disclaimer.
-# 
-# * Redistributions in binary form must reproduce the above copyright
-# notice, this list of conditions and the following disclaimer in the
-# documentation and/or other materials provided with the distribution.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE 
-# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
-# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
-# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
-# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
-# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
-# POSSIBILITY OF SUCH DAMAGE.
-#
-# Last modified 10 March 2005.
+# Last modified 14 December 2006.
 #
 
 . ../makeprogs/paths.sh
@@ -53,7 +35,7 @@ for f in pprprox.allow ppop.allow ppad.allow ppuser.allow
 #===========================================================================
 echo "Making missing configuration files from the samples..."
 
-for f in ppr.conf uprint-remote.conf lprsrv.conf
+for f in ppr.conf lprsrv.conf
 	do
 	./puts "  $f..."
 	if [ ! -f $RPM_BUILD_ROOT$CONFDIR/$f ]
