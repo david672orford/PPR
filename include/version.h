912a795d707099954dff0bc25fafc49793274376
chappell <>
2008-06-12 20:51:01 +0000

    no_comment_raven

--- include/version.h
+++ include/version.h
@@ -1,6 +1,6 @@
 #define SHORT_VERSION "2.00a1"
-#define REVISION_DATE "1 June 2007"
-#define COPYRIGHT_YEARS "1995--2007"
+#define REVISION_DATE "23 May 2008"
+#define COPYRIGHT_YEARS "1995--2008"
 
 #define VERSION "PPR version "SHORT_VERSION", "REVISION_DATE" (built "__DATE__", "__TIME__")."
 #define COPYRIGHT "Copyright "COPYRIGHT_YEARS", Trinity College Computing Center."
