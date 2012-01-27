dd52f82f52a0a1ed095569be64fcaf2c6bb576d4
chappell <>
2012-01-27 17:13:39 +0000

    *** empty log message ***

--- include/version.h
+++ include/version.h
@@ -1,6 +1,6 @@
 #define SHORT_VERSION "2.00a1"
-#define REVISION_DATE "4 March 2011"
-#define COPYRIGHT_YEARS "1995--2010"
+#define REVISION_DATE "27 January 2012"
+#define COPYRIGHT_YEARS "1995--2012"
 
 #define VERSION "PPR version "SHORT_VERSION", "REVISION_DATE" (built "__DATE__", "__TIME__")."
 #define COPYRIGHT "Copyright "COPYRIGHT_YEARS", Trinity College Computing Center."
